de102af21b577fc356885ba06c02aa913bdfc9c6
Lare1426 <laurikj.virta@gmail.com>
2023-01-21 21:49:32 +0200

    Changed the static file path to a relative one

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 import express from "express";
-
+import path from "path";
+import { fileURLToPath } from "url";
 const server = express();
 const PORT = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-server.get("/", (req, res) => {
-  res.send();
-});
-
-server.use(express.static("../client"));
+server.use(express.static(path.join(__dirname, "../client")));
 
 server.listen(PORT, () => {
   console.log("Listening to port", PORT);
