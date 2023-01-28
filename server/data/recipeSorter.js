bc3ab38e44602d591add095ea6f47516578f81ec
Lare1426 <laurikj.virta@gmail.com>
2023-01-28 15:15:33 +0200

    sorted out alternate recipes and base recipes

---
+++ server/data/recipeSorter.js
@@ -0,0 +1,27 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const data = JSON.parse(fs.readFileSync(path.join(__dirname, "recipes.json")));
+
+let baseRecipes = {};
+let alternateRecipes = {};
+
+for (const [key, value] of Object.entries(data)){
+  for (const recipe of value){
+    if (recipe["name"].includes("Alternate")){
+      alternateRecipes[key] = [];
+      alternateRecipes[key].push(recipe);
+    } else {
+      baseRecipes[key] = [];
+      baseRecipes[key].push(recipe);
+    }
+  }
+}
+
+let baseRecipesData = JSON.stringify(baseRecipes, null, 2);
+fs.writeFileSync("baseRecipes.json", baseRecipesData);
+
+let alternateRecipesData = JSON.stringify(alternateRecipes, null, 2);
+fs.writeFileSync("alternateRecipes.json", alternateRecipesData);
