7603d92ff3f27c6cd210993279b763257e710391
Lare1426 <laurikj.virta@gmail.com>
2023-01-29 17:03:00 +0200

    fixed showing only 1 recipe of each product to user

--- client/script.js
+++ client/script.js
@@ -1,45 +1,47 @@
 const [contentBox] = document.getElementsByClassName("content");
 
-const constructList = (recipes) => {
+const constructList = (data) => {
   contentBox.innerHTML = "";
-  for (const [key, value] of Object.entries(recipes)){
-    const list = document.createElement("ul");
-    list.classList.add("list");
+  for (const [item, recipes] of Object.entries(data)) {
+    for (const recipe of recipes) {
+      const list = document.createElement("ul");
+      list.classList.add("list");
 
-    const name = document.createElement("li");
-    name.innerText = `name: ${value[0]["name"]}`;
-    const time = document.createElement("li");
-    time.innerText = `time to process: ${value[0]["time"]}`;
+      const name = document.createElement("li");
+      name.innerText = `name: ${recipe["name"]}`;
+      const time = document.createElement("li");
+      time.innerText = `time to process: ${recipe["time"]}`;
 
-    const productsLi = document.createElement("li");
-    productsLi.innerText = "products:";
-    const productsUl = document.createElement("ul");
-    for (const product of value[0]["products"]){
-      const item = document.createElement("li");
-      item.innerText = `${product["item"]} (${product["amount"]})`;
+      const productsLi = document.createElement("li");
+      productsLi.innerText = "products:";
+      const productsUl = document.createElement("ul");
+      for (const product of recipe["products"]) {
+        const item = document.createElement("li");
+        item.innerText = `${product["item"]} (${product["amount"]})`;
 
-      productsUl.append(item);
+        productsUl.append(item);
+      }
+      productsLi.append(productsUl);
+
+      const ingredientsLi = document.createElement("li");
+      ingredientsLi.innerText = "ingredients:";
+      const ingredientsUl = document.createElement("ul");
+      for (const ingredient of recipe["ingredients"]) {
+        const item = document.createElement("li");
+        item.innerText = `${ingredient["item"]} (${ingredient["amount"]})`;
+
+        ingredientsUl.append(item);
+      }
+      ingredientsLi.append(ingredientsUl);
+
+      list.append(name, time, productsLi, ingredientsLi);
+      contentBox.append(list);
     }
-    productsLi.append(productsUl);
-
-    const ingredientsLi = document.createElement("li");
-    ingredientsLi.innerText = "ingredients:";
-    const ingredientsUl = document.createElement("ul");
-    for (const ingredient of value[0]["ingredients"]){
-      const item = document.createElement("li");
-      item.innerText = `${ingredient["item"]} (${ingredient["amount"]})`;
-
-      ingredientsUl.append(item);
-    }
-    ingredientsLi.append(ingredientsUl);
-
-    list.append(name, time, productsLi, ingredientsLi);
-    contentBox.append(list);
   }
-}
+};
 
 const displayRecipes = async (type) => {
   const response = await fetch(`http://localhost:3000/${type}Recipes.json`);
   const data = await response.json();
-  constructList(data); 
-}
+  constructList(data);
+};
