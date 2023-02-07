d70edb1cc8ebcbded7d02bd54743ea1c47d70dfb
Lare1426 <laurikj.virta@gmail.com>
2023-02-07 21:17:39 +0200

    Updated the recipes ingredients and products to have correct item names

--- server/data/dataSorter.js
+++ server/data/dataSorter.js
@@ -8,21 +8,39 @@ const data = JSON.parse(fs.readFileSync(path.join(__dirname, "data.json")));
 const newData = {};
 let tempValues = [];
 
+let count = 0;
 for (const [key, value] of Object.entries(data)) {
+  if (count == 2) {
+    break;
+  }
   newData[key] = {};
   tempValues.push(value);
-  break;
+  count++;
 }
 
 for (const [k, v] of Object.entries(tempValues[0])) {
   if (v["inMachine"]) {
-    newData["recipes"][v["slug"]] = {
+    newData["recipes"][v["name"]] = {
       name: v["name"],
       time: v["time"],
-      ingredients: v["ingredients"],
-      products: v["products"],
+      ingredients: [],
+      products: [],
       producedIn: v["producedIn"],
     };
+
+    for (const ingredient of v["ingredients"]) {
+      newData["recipes"][v["name"]]["ingredients"].push({
+        item: tempValues[1][ingredient["item"]]["name"],
+        amount: ingredient["amount"],
+      });
+    }
+
+    for (const product of v["products"]) {
+      newData["recipes"][v["name"]]["products"].push({
+        item: tempValues[1][product["item"]]["name"],
+        amount: product["amount"],
+      });
+    }
   }
 }
 
