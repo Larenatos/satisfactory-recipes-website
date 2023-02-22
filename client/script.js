aca8226eb05ca594f9176e140f28876a8fba1698
Lare1426 <laurikj.virta@gmail.com>
2023-02-22 15:46:29 +0200

    making more descriptive variable names

--- client/script.js
+++ client/script.js
@@ -1,92 +1,86 @@
 const baseUrl = "/satisfactory-recipes";
 
-const [resultBox] = document.getElementsByClassName("search-results");
-const [errorText] = document.getElementsByClassName("error");
+const [resultDiv] = document.getElementsByClassName("result");
+const [errorP] = document.getElementsByClassName("error");
+const productInput = document.getElementById("product-input");
 
 const displayRecipeList = (data) => {
-  resultBox.innerHTML = "";
+  resultDiv.innerHTML = "";
 
   for (const [item, recipes] of Object.entries(data)) {
-    const productName = document.createElement("h2");
-    productName.innerText = item;
-    resultBox.append(productName);
+    const productH2 = document.createElement("h2");
+    productH2.innerText = item;
 
-    const recipeDivs = document.createElement("div");
+    const recipesDiv = document.createElement("div");
 
     for (const recipe of recipes) {
-      const recipeName = document.createElement("h3");
-      recipeName.innerText = recipe.name;
+      const recipeH3 = document.createElement("h3");
+      recipeH3.innerText = recipe.name;
 
-      const list = document.createElement("ul");
-      list.classList.add("top-level-list");
+      const recipeUl = document.createElement("ul");
+      recipeUl.classList.add("top-level-list");
 
-      const name = document.createElement("li");
-      name.innerText = `name: ${recipe.name}`;
+      const timeLi = document.createElement("li");
+      timeLi.innerText = `Crafting time: ${recipe.time}s`;
 
-      const time = document.createElement("li");
-      time.innerText = `time to process: ${recipe.time}s`;
-
-      const alternate = document.createElement("li");
-      alternate.innerText = `alternate: ${recipe.alternate}`;
-
-      const producedIn = document.createElement("li");
-      producedIn.innerText = "produced in:";
+      const producedInLi = document.createElement("li");
+      producedInLi.innerText = "Produced in:";
 
       const producedInUl = document.createElement("ul");
       for (const machine of recipe.producedIn) {
-        const machineElement = document.createElement("li");
-        machineElement.innerText = machine;
+        const machineLi = document.createElement("li");
+        machineLi.innerText = machine;
 
-        producedInUl.append(machineElement);
+        producedInUl.append(machineLi);
       }
-      producedIn.append(producedInUl);
+      producedInLi.append(producedInUl);
 
       const productsLi = document.createElement("li");
-      productsLi.innerText = "products:";
+      productsLi.innerText = "Products:";
 
       const productsUl = document.createElement("ul");
       for (const product of recipe.products) {
-        const productElement = document.createElement("li");
-        productElement.innerText = `${product.item} (${product.amount})`;
+        const productLi = document.createElement("li");
+        productLi.innerText = `${product.item} (${product.amount})`;
 
-        productsUl.append(productElement);
+        productsUl.append(productLi);
       }
       productsLi.append(productsUl);
 
       const ingredientsLi = document.createElement("li");
-      ingredientsLi.innerText = "ingredients:";
+      ingredientsLi.innerText = "Ingredients:";
 
       const ingredientsUl = document.createElement("ul");
       for (const ingredient of recipe.ingredients) {
-        const ingredientElement = document.createElement("li");
-        ingredientElement.innerText = `${ingredient.item} (${ingredient.amount})`;
+        const ingredientLi = document.createElement("li");
+        ingredientLi.innerText = `${ingredient.item} (${ingredient.amount})`;
 
-        ingredientsUl.append(ingredientElement);
+        ingredientsUl.append(ingredientLi);
       }
       ingredientsLi.append(ingredientsUl);
 
-      list.append(name, time, alternate, producedIn, productsLi, ingredientsLi);
-      recipeDivs.append(recipeName, list);
+      recipeUl.append(timeLi, producedInLi, productsLi, ingredientsLi);
+      recipesDiv.append(recipeH3, recipeUl);
     }
-    resultBox.append(recipeDivs);
+    resultDiv.append(productH2, recipesDiv);
   }
 };
 
-const displayRecipes = async (type) => {
+const displayBulkRecipes = async (type) => {
   const response = await fetch(`${baseUrl}/${type}Recipes.json`);
   const data = await response.json();
   displayRecipeList(data);
 };
 
 const displaySearchResults = async () => {
-  errorText.innerText = "";
-  const input = document.getElementById("product-input").value;
+  errorP.innerText = "";
+  const input = productInput.value;
 
   const response = await fetch(`${baseUrl}/search?key=${input}`);
 
   if (response.status == 400) {
     const { message } = await response.json();
-    errorText.innerText = message;
+    errorP.innerText = message;
     return;
   }
 
@@ -94,20 +88,20 @@ const displaySearchResults = async () => {
   displayRecipeList(data);
 };
 
-const displayAllKeys = async () => {
+const displayAllProducts = async () => {
   const response = await fetch(`${baseUrl}/products`);
   const data = await response.json();
-  resultBox.innerHTML = "";
+  resultDiv.innerHTML = "";
 
   for (const item of data) {
     const key = document.createElement("h4");
     key.innerText = item;
 
     key.addEventListener("click", () => {
-      document.getElementById("product-input").value = item;
+      productInput.value = item;
       displaySearchResults();
     });
 
-    resultBox.append(key);
+    resultDiv.append(key);
   }
 };
